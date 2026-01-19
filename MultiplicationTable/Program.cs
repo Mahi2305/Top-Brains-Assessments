bc14cd7f098b637d5f8e096118939f26713086f3
Mahi2305 <114794143+Mahi2305@users.noreply.github.com>
2026-01-19 14:36:56 +0530

    Add files via upload

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static int[] GetMultiplicationRow(int n, int upto)
+    {
+        int[] row = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            row[i - 1] = n * i;
+        }
+
+        return row;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter upto: ");
+        int upto = int.Parse(Console.ReadLine());
+
+        int[] result = GetMultiplicationRow(n, upto);
+
+        Console.WriteLine("\nMultiplication Row:");
+        foreach (int x in result)
+        {
+            Console.Write(x + " ");
+        }
+    }
+}
+
